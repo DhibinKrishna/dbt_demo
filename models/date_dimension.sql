86e5e2eeb6ed9be4e6d68e728f40a2aea65ac175
dhibin krishna <dhibinkrishna@gmail.com>
2026-01-29 10:31:49 +0000

    date utils

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,10 +1,14 @@
 with cte as (
     select to_timestamp(started_at) as started_at,
     date(to_timestamp(started_at)) as date_started_at,
-    case when dayname(to_timestamp(started_at)) in ('Sat', 'Sun') // macro
-        then 'Weekend'
-        else 'Business day'
-        end as day_type,
+    hour(to_timestamp(started_at)) as hour_started_at,
+    -- case when dayname(to_timestamp(started_at)) in ('Sat', 'Sun') // macro
+    --     then 'Weekend'
+    --     else 'Business day'
+    --     end as day_type,
+    {{
+        weekday('started_at')
+    }},
     case when month(to_timestamp(started_at)) in (12, 1, 2)
         then 'Winter'
         when month(to_timestamp(started_at)) in (3, 4, 5)
